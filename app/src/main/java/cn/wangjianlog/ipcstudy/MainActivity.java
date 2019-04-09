ce1c6cbb2ee450a3124bed2cf6706de35e4d7787
王健 <fighter576133402@gmail.com>
2019-04-09 14:50:39 +0800

    test

--- app/src/main/java/cn/wangjianlog/ipcstudy/MainActivity.java
+++ app/src/main/java/cn/wangjianlog/ipcstudy/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Test", null).show();
             }
         });
 
